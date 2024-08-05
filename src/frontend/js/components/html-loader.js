f10188789ec5c9c6418114384219ce903f4487ef
Gustavo Rutkowski <rutkowskigustavo@gmail.com>
2024-08-05 12:30:25 -0300

    Adicionando o HTML Loader.

--- src/frontend/js/components/html-loader.js
+++ src/frontend/js/components/html-loader.js
@@ -1,5 +1,19 @@
 class HTMLLoader {
+    constructor ({ id, items, target }) {
+        if (typeof id != 'number') {
+            console.error('id is not a number.');
+            throw new TypeError('id is not a number.');
+        };
 
+        if (!Array.isArray(items)) {
+            console.error('items is not a array.');
+            throw new TypeError('items is not a array.');
+        };
+
+        this.id = id;
+        this.items = items;
+        this.target = target;
+    };
 }
 
 export default HTMLLoader;
